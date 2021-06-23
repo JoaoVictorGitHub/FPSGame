f3c6cc8cccc259ab3afdbdca8e46a1c6e34ff814
JoaoVictorGitHub <86333334+JoaoVictorGitHub@users.noreply.github.com>
2021-06-23 03:09:13 +0100

    Add files via upload

---
+++ Gun.cs
@@ -0,0 +1,111 @@
+﻿using System.Diagnostics;
+using System.Runtime;
+using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float ImpactForce = 30f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
+    public Animator animator;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+
+    }
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            animator.SetBool("Aiming", true);
+            //isAiming = true;           
+        }
+        if (Input.GetButtonUp("Fire2"))
+        {
+            animator.SetBool("Aiming", false);
+            //isAiming = false;
+        }
+
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        UnityEngine.Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
+    void Shoot()
+    {
+
+        muzzleFlash.Play();
+
+        currentAmmo--;
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            UnityEngine.Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * ImpactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
